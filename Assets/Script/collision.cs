7fea32c7bebfdf3d77acb61eb303f9088c723357
AadarshaSadan <bajagainsadan@gmail.com>
2018-07-27 00:50:34 +0545

    with destroy

---
+++ Assets/Script/collision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour {
+    public GameObject tower;
+    Transform t;
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       double x = t.position.y;
+        print(x);
+    }
+}
